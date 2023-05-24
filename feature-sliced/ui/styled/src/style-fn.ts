0730efe79e32f1bd484d5831abddc0e1b744557d
Danil Karpenko <limpix31@gmail.com>
2023-05-24 14:00:13 +0200

    fix: type

--- feature-sliced/ui/styled/src/style-fn.ts
+++ feature-sliced/ui/styled/src/style-fn.ts
@@ -1,4 +1,4 @@
 import type { InterpolationPrimitive } from '@emotion/serialize'
 import type { Theme } from '@emotion/react'
 
-export type StyleFn<T = {}> = (props: (Record<any, any> | { theme: Theme }) & T) => InterpolationPrimitive
+export type StyleFn<T = {}> = (props: { theme: Theme } & T) => InterpolationPrimitive
